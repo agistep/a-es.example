f928f4f4517a7fe1bb0cabb927269e577a4ad0dc
yuna <yoonha.yuna.dev@gmail.com>
2023-12-10 16:06:00 +0900

    remove the sequence of an aggregateId when an aggregate is cleared #49

--- event/core/src/test/java/io/agistep/event/ThreadLocalEventSeqHolderTest.java
+++ event/core/src/test/java/io/agistep/event/ThreadLocalEventSeqHolderTest.java
@@ -1,8 +1,12 @@
 package io.agistep.event;
 
+import io.agistep.foo.Foo;
+import io.agistep.foo.FooCreated;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.agistep.event.test.EventFixtureBuilder.anEventWith;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ThreadLocalEventSeqHolderTest {
@@ -26,5 +30,36 @@ class ThreadLocalEventSeqHolderTest {
 
         assertThat(EventSource.getLatestSeqOf(1L)).isEqualTo(-1);
 
+    }
+
+    @Test
+    @DisplayName("remove the sequence of an aggregateId when aggregate is cleared from EventSource")
+    void clear() {
+        Foo aggregate1 = new Foo();
+        EventSource.reorganize(aggregate1, anEventWith(new FooCreated()));
+
+        Foo aggregate2 = new Foo();
+        EventSource.reorganize(aggregate2, anEventWith(new FooCreated()));
+
+        EventSource.clear(aggregate1);
+
+        assertThat(EventSource.getLatestSeqOf(aggregate1)).isEqualTo(-1);
+        assertThat(EventSource.getLatestSeqOf(aggregate2)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("remove the sequence of an aggregateId when aggregate is cleared")
+    void clear2() {
+        Foo aggregate1 = new Foo();
+        EventSource.reorganize(aggregate1, anEventWith(new FooCreated()));
+
+        Foo aggregate2 = new Foo();
+        EventSource.reorganize(aggregate2, anEventWith(new FooCreated()));
+
+        var sut = ThreadLocalEventSeqHolder.instance();
+        sut.clear(aggregate1);
+
+        assertThat(sut.getSeq(aggregate1.getId())).isEqualTo(-1);
+        assertThat(sut.getSeq(aggregate2.getId())).isEqualTo(0);
     }
 }
