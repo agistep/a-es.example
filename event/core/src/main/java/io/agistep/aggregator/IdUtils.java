99d4dc500b520cc1d16c75392640f16e13f58de2
myangw <kmh4503@gmail.com>
2024-01-28 21:50:25 +0900

    remove previous idUtils methods

--- event/core/src/main/java/io/agistep/aggregator/IdUtils.java
+++ event/core/src/main/java/io/agistep/aggregator/IdUtils.java
@@ -4,82 +4,12 @@ package io.agistep.aggregator;
 import io.agistep.identity.IdentifierProvider;
 import io.agistep.identity.spi.IdentifierProviderFactory;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-
-import static java.lang.String.format;
-
 public final class IdUtils {
 
 	private static final IdentifierProvider IDENTIFIER_PROVIDER = IdentifierProviderFactory.load().get();
-	private static final Class<?>[] SUPPORTED_TYPES = { long.class, Long.class };
-	private static final String NAME_OF_ID_FIELD = "id";
-
-	public static long idOf(Object aggregate) {
-
-		Field field = getIdField(aggregate);
-
-		try{
-
-			Object id = field.get(aggregate);
-			if(field.getType().isPrimitive() &&  Long.valueOf(0).equals(id)) {
-				throw new IllegalAggregateIdException(format("Primitive Type Int and long must not have 0(zero). :%s", aggregate.getClass().getName()));
-			}
-
-			if(!field.getType().isPrimitive() && id == null)  {
-				throw new IllegalAggregateIdException(format("An Id must not be null. :%s", aggregate.getClass().getName()));
-			}
-
-			return (Long) id;
-		} catch (IllegalAccessException e) {
-			throw new IllegalAggregateIdException(e.getMessage(), e);
-		}
-	}
 
 	public static long idOf(Aggregate aggregate) {
 		return aggregate.getId();
-	}
-
-
-	private static Field getIdField(Object aggregate) {
-		Field field;
-		try {
-			field = aggregate.getClass().getDeclaredField(NAME_OF_ID_FIELD);
-		} catch (NoSuchFieldException e) {
-			Field[] superClassFields = aggregate.getClass().getSuperclass().getDeclaredFields();
-			if (superClassFields.length == 0) {
-				throw new IllegalAggregateIdException("Aggregate Must Have 'id' field.",e);
-			}
-
-			field = superClassFields[0];
-		} catch (Exception e) {
-			throw new IllegalAggregateIdException("Aggregate Must Have 'id' field.",e);
-		}
-
-		if (isNotSupport(field)) {
-			throw new IllegalAggregateIdException("An ID field is applied should be one of the following types: int, long, Long, Integer, String",null);
-		}
-
-		field.setAccessible(true);
-		return field;
-	}
-
-	private static boolean isNotSupport(Field field) {
-		return Arrays.stream(SUPPORTED_TYPES).noneMatch(t -> t.getTypeName().equals(field.getType().getTypeName()));
-	}
-
-	public static boolean notAssignedIdOf(Object aggregate) {
-		Field field = getIdField(aggregate);
-		try {
-			Object id = field.get(aggregate);
-			if (field.getType().isPrimitive() && ((Number) id).longValue() == 0L) {
-				return true;
-			}
-
-			return !field.getType().isPrimitive() && id == null;
-		} catch (IllegalAccessException e) {
-			throw new IllegalAggregateIdException(e.getMessage(), e);
-		}
 	}
 
 	public static boolean notAssignedIdOf(Aggregate aggregate) {
--- event/core/src/test/java/io/agistep/aggregator/IdUtilsTest.java
+++ event/core/src/test/java/io/agistep/aggregator/IdUtilsTest.java
@@ -3,49 +3,29 @@ package io.agistep.aggregator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-
 import static io.agistep.aggregator.IdUtils.idOf;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class IdUtilsTest {
 
     final static A A = new A();
     final static B B = new B();
-    final static F F = new F();
-    final static G G = new G();
-    final static H H = new H();
-    final static I I = new I();
 
-    static class A { long id = 1L;}
-    static class B { Long id = 2L;}
-    static class F { }
-    static class G { BigDecimal id;}
-    static class H { long id;}
-    static class I { Long id;}
+    static class A implements Aggregate {
+        long id = 1L;
+        @Override
+        public long getId() {
+            return id;
+        }
+    }
+    static class B implements Aggregate {
+        long id;
 
-    @Test
-    @DisplayName("Exception: Illegal AggregateId")
-    void idOf0() {
-        assertThatThrownBy(()->idOf(F))
-                .isInstanceOf(IllegalAggregateIdException.class)
-                .hasMessage("Aggregate Must Have 'id' field. :%s", F.getClass().getName());
-
-        assertThatThrownBy(()->idOf(G))
-                .isInstanceOf(IllegalAggregateIdException.class)
-                .hasMessage(
-                        "An ID field applied should be one of the following types: " +
-                        "long, Long. :%s", G.getClass().getName());
-
-        assertThatThrownBy(()->idOf(H))
-                .isInstanceOf(IllegalAggregateIdException.class)
-                .hasMessage("Primitive Type Int and long must not have 0(zero). :%s", H.getClass().getName());
-
-        assertThatThrownBy(()->idOf(I))
-                .isInstanceOf(IllegalAggregateIdException.class)
-                .hasMessage("An Id must not be null. :%s", I.getClass().getName());
+        @Override
+        public long getId() {
+            return id;
+        }
     }
 
     @Test
@@ -58,7 +38,6 @@ class IdUtilsTest {
     @Test
     void assertThatNotAssignIdOf() {
         assertThat(IdUtils.notAssignedIdOf(A)).isFalse();
-        assertThat(IdUtils.notAssignedIdOf(H)).isTrue();
-        assertThat(IdUtils.notAssignedIdOf(I)).isTrue();
+        assertThat(IdUtils.notAssignedIdOf(IdUtilsTest.B)).isTrue();
     }
 }
